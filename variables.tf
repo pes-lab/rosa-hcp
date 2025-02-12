f989dcbefcbb984ecf324597e59ec40890e0e191
woodhead66 <woodhead66@163.com>
2025-02-12 11:36:04 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -77,17 +77,17 @@ variable "vpc_cidr_block" {
 #  default     = "10.0.0.0/16"
 }
 
-variable "private_subnet_cidrs" {
-  type        = list(any)
-  description = "The CIDR blocks to use for the private subnets"
+#variable "private_subnet_cidrs" {
+#  type        = list(any)
+#  description = "The CIDR blocks to use for the private subnets"
 #  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-}  
+#}  
 
-variable "public_subnet_cidrs" {
-  type        = list(any)
-  description = "The CIDR blocks to use for the public subnets"
+#variable "public_subnet_cidrs" {
+#  type        = list(any)
+#  description = "The CIDR blocks to use for the public subnets"
 #  default     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-}
+#}
 
 variable "single_nat_gateway" {
   type        = bool
