bb04cf6b852303a2d309818e7079cad89f4c11e0
woodhead66 <woodhead66@163.com>
2025-02-12 11:40:31 +0800

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -22,8 +22,8 @@ module "vpc" {
   cidr  = var.vpc_cidr_block
 
   azs             = local.region_azs
-  private_subnets = var.multi_az ? var.private_subnet_cidrs : [var.private_subnet_cidrs[0]]
-  public_subnets  = var.multi_az ? var.public_subnet_cidrs : [var.public_subnet_cidrs[0]]
+  #private_subnets = var.multi_az ? var.private_subnet_cidrs : [var.private_subnet_cidrs[0]]
+  #public_subnets  = var.multi_az ? var.public_subnet_cidrs : [var.public_subnet_cidrs[0]]
 
   enable_nat_gateway   = true
   single_nat_gateway   = var.single_nat_gateway
