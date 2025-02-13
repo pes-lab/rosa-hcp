be95edeb9ff4451efbfeb21ab77ca745f4d4a18a
woodhead66 <woodhead66@163.com>
2025-02-13 08:35:14 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -82,7 +82,7 @@ module "rosa-hcp" {
   aws_subnet_ids         = var.create_vpc ? var.private_cluster ? module.vpc[0].private_subnets : concat(module.vpc[0].public_subnets, module.vpc[0].private_subnets) : var.aws_subnet_ids
   create_account_roles   = true
   create_operator_roles  = true
-#  compute_machine_type   = var.compute_machine_type
+  compute_machine_type   = var.compute_machine_type
   operator_role_prefix   = var.operator_role_prefix
 # Optional: Configure a cluster administrator user 
 #
